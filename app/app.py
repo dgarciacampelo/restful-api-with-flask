65e6b724b39d62d579d0dd6165b0eccfa99c3cff
Daniel García Campelo <dgarciacampelo@gmail.com>
2023-11-04 16:02:29 +0100

    after completing chapter 1: getting started

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -9,6 +9,12 @@ def hello_world():
     return "Hello World!"
 
 
+@app.route("/super-simple")
+def super_simple():
+    return jsonify(message="Hello from the Planetary API, nice."), 200
+
+
 if __name__ == "__main__":
     # * Runs a a development server on http://127.0.0.1:5000 by default.
-    app.run()
+    # * With debug=True, the server reloads itself each time the code changes.
+    app.run(debug=True)
