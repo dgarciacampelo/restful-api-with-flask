ff9d923eb7eafbf024613ca1a0d7a34d75346678
Daniel García Campelo <dgarciacampelo@gmail.com>
2023-11-04 22:01:11 +0100

    after completing chapter 3

--- app/app.py
+++ app/app.py
@@ -1,7 +1,17 @@
 from flask import Flask, jsonify, request
-
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "planets.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+
+# Database setup and model definitions.
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 @app.route("/")
@@ -40,6 +50,125 @@ def url_variables(name: str, age: int):
         return jsonify(message="Sorry " + name + ", you are not old enough."), 401
     else:
         return jsonify(message="Welcome " + name + ", you are old enough!"), 200
+
+
+@app.route("/planets", methods=["GET"])
+def planets():
+    planets_list = Planet.query.all()
+    planets = planets_schema.dump(planets_list)
+    return jsonify(planets)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "first_name", "last_name", "email", "password")
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "planet_id",
+            "planet_name",
+            "planet_type",
+            "home_star",
+            "mass",
+            "radius",
+            "distance",
+        )
+
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+
+
+# Commands for the terminal, to manage the database.
+@app.cli.command("db_create")
+def db_create():
+    db.create_all()
+    print("Database created!")
+
+
+@app.cli.command("db_drop")
+def db_drop():
+    db.drop_all()
+    print("Database dropped!")
+
+
+@app.cli.command("db_seed")
+def db_seed():
+    db_seed_planets()
+    db_seed_user()
+    db.session.commit()
+    print("Database seeded!")
+
+
+def db_seed_planets():
+    mercury = Planet(
+        planet_name="Mercury",
+        planet_type="Class D",
+        home_star="Sol",
+        mass=2.258e23,
+        radius=1516,
+        distance=35.98e6,
+    )
+
+    venus = Planet(
+        planet_name="Venus",
+        planet_type="Class K",
+        home_star="Sol",
+        mass=4.867e24,
+        radius=3760,
+        distance=67.24e6,
+    )
+
+    earth = Planet(
+        planet_name="Earth",
+        planet_type="Class M",
+        home_star="Sol",
+        mass=5.972e24,
+        radius=3959,
+        distance=92.96e6,
+    )
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+
+def db_seed_user():
+    # TODO: Hash the password.
+    test_user = User(
+        first_name="William",
+        last_name="Herschel",
+        email="test@test.com",
+        password="P@ssw0rd",
+    )
+
+    db.session.add(test_user)
 
 
 if __name__ == "__main__":
